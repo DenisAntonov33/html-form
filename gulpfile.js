2a232eebc0e94dffbfbc63e073a7a12bdf2da429
denis <danchic95@gmail.com>
2018-12-13 20:01:35 +0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var browserSync = require("browser-sync");
+
+
+gulp.task("html", function() {
+	return gulp.src("src/index.html")
+	.pipe(gulp.dest("dist"))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task("style", function () {
+	return gulp.src("src/styles/**/*")
+	.pipe(sass())
+	.pipe(gulp.dest("dist/styles"))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task("server", function () {
+	browserSync({
+		server: {
+			baseDir: "dist"
+		}
+	});
+});
+
+gulp.task("watch", function() {
+	gulp.watch("src/index.html", gulp.parallel("html"));
+	gulp.watch("src/styles/**/*", gulp.parallel("style"));
+})
+
+gulp.task("default", gulp.parallel("watch", "html", "style", "server"))
